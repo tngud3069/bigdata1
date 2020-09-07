ba60f9c2b18a3f0cbba0f3a832158110816af7f5
SMHRD <SMHRD@DESKTOP-QRHV0RR>
2020-09-07 16:44:41 +0900

    수정완료

--- webmvc/src/kr/smhrd/MyUtil.java
+++ webmvc/src/kr/smhrd/MyUtil.java
@@ -4,9 +4,9 @@ public class MyUtil {
 	// 1~10까지의 총합을 구하는 동작
 	public void hap() {
 		int sum = 0;
-		for(int i = 1; i<=10; i++) {
+		for(int i = 1; i<=100; i++) {
 			sum+=i;
 		}//00아 여기수정!
-		
+		return sum;
 	}
 }
